47d3d2ad2a4fd83a6701a998ea06471e02305e6a
TechYash-bit <harneyash7418@gmail.com>
2025-04-26 17:56:27 +0530

    Add files via upload
    starting date 25 april 2025

---
+++ program/day1/variable.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//creating a string variable
+	var name string = "yash niranjan harne"
+	fmt.Println(name)
+
+	var name2="yash"
+	fmt.Println(name2)
+
+	name3:="harne"
+	fmt.Println(name3)
+
+	//creating an int variable
+	var age int=12
+	fmt.Println(age)
+
+	var age2=123
+	fmt.Println(age2)
+
+	age3:=1234
+
+	fmt.Println(age3)
+	//craeting an float variable
+	var  price float32=12.36
+	fmt.Println(price)
+
+	var price2=13.36
+	fmt.Println(price2)
+
+	price3:=123.36
+	fmt.Println(price3)
+
+}
